a738cbbbb72221155b3700425379ee3e8a260b4b
Ayoub Ogbi <aogbi@e1r4p6.1337.ma>
2024-10-25 14:03:44 +0100

    add some lines

--- includes/minirt.h
+++ includes/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aogbi <aogbi@student.1337.ma>              +#+  +:+       +#+        */
+/*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:52:26 by aogbi             #+#    #+#             */
-/*   Updated: 2024/10/14 17:13:50 by aogbi            ###   ########.fr       */
+/*   Updated: 2024/10/25 13:58:11 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,28 @@
 
 #include <stdio.h>
 #include "../Libft/libft.h"
+#include <mlx.h>
+#include <math.h>
+
+#define WIDTH 900
+#define HEIGHT 600
+
+typedef struct s_img
+{
+    void	*mlx_img;
+    char	*addr;
+    int		bpp;
+    int		line_len;
+    int		endian;
+    int        width;
+    int        height;
+}	t_img;
+
+typedef struct s_data
+{
+    void	*mlx_ptr;
+    void	*win_ptr;
+    t_img   img;
+}	t_data;
 
 #endif
--- vec3.c
+++ vec3.c
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   vec3.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aogbi <aogbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/14 16:52:14 by aogbi             #+#    #+#             */
-/*   Updated: 2024/10/25 14:00:44 by aogbi            ###   ########.fr       */
+/*   Created: 2024/10/25 13:58:38 by aogbi             #+#    #+#             */
+/*   Updated: 2024/10/25 13:58:54 by aogbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/minirt.h"
-
-int main()
-{
-    t_data data;
-	data.mlx_ptr = mlx_init();
-    data.win_ptr = mlx_new_window(data.mlx_ptr, WIDTH, HEIGHT, "MiniRT");
-    data.img.mlx_img = mlx_new_image(data.mlx_ptr, WIDTH, HEIGHT);
-    data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
-    mlx_loop(data.mlx_ptr);
-    return (0);
-}
